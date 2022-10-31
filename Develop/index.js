f1fa1abdb5eb10d294a6d83b09a287e2c90d2870
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-10-30 19:17:47 -0500

    adds inquirer questions and writeFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+     {
+      type: 'input',
+      name: 'title',
+      message: 'Please enter the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please enter a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please enter installation instructions for your project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please enter usage information for your project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter guidelines for project contributions:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter the tests for your project:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for your project:',
+        choices: ['MIT', 'Apache', 'GNU GPL v3', 'Perl']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your GitHub Username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address:',
+    },
+  ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        console.log( err ? err : "File successfully written!");
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+/*
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = ({ name, location, github, linkedin }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">I am from ${location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${github}</li>
+      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
+  */
